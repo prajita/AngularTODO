3f0b77c70e98ce9b1eb8d93c8e80b3d06173abb7
prajitas <prajitas@vmware.com>
2023-10-21 20:56:55 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { repeat } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  constructor(){
+    setTimeout(()=>{
+      this.title = 'prajitas-todo-list';
+    }, 2000);
+  }
 }
